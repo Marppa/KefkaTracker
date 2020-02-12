3374d2578881fc8e75cf8cf81e127e175230826a
Marppa <maria.annala.91@gmail.com>
2020-02-12 18:14:22 +0200

    added behaviour and poop properties to KefkaTrip class

--- kefka.py
+++ kefka.py
@@ -1,10 +1,16 @@
 class KefkaTrip:
-    time = 15
-    def __init__(self, time_parameter):
-        self.time = time_parameter
-        
+    def __init__(self, time, behaviour, poop):
+        self.time = time
+        self.behaviour = behaviour
+        self.poop = poop
 
-trips = [KefkaTrip(20), KefkaTrip(40)]
+trips = [KefkaTrip(20, 'hermostunut', True), KefkaTrip(40, 'tohko', False)]
 
 for trip in trips:
     print('kefkan retki kesti ' + str(trip.time))
+    print('kefka oli ' + str(trip.behaviour))
+    if trip.poop:
+        print('tuli kakka')
+    else:
+        print('ei kakkaa')
+    print('')
